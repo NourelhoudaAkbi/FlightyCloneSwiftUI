2777dabb3b1a19ad3e50e26125853d6ca0b8bf3b
NourelhoudaAkbi <nourelhoudaakbi@gmail.com>
2024-09-19 12:42:40 +0100

    Initial Commit

---
+++ FlightyCloneSwiftUI/FlightyCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightyCloneSwiftUIApp.swift
+//  FlightyCloneSwiftUI
+//
+//  Created by Nour el houda Akbi on 19/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightyCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlightyCloneSwiftUITests/FlightyCloneSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  FlightyCloneSwiftUITests.swift
+//  FlightyCloneSwiftUITests
+//
+//  Created by Nour el houda Akbi on 19/9/2024.
+//
+
+import Testing
+@testable import FlightyCloneSwiftUI
+
+struct FlightyCloneSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
