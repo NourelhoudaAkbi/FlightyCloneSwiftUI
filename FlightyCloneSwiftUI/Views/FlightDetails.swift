efb6c0474fa1bda105e43f3a316b99b7d2042e94
NourelhoudaAkbi <nourelhoudaakbi@gmail.com>
2024-09-20 14:32:06 +0100

    added ArrivalForecast

--- FlightyCloneSwiftUI/Views/FlightDetails.swift
+++ FlightyCloneSwiftUI/Views/FlightDetails.swift
@@ -24,6 +24,7 @@ struct FlightDetails: View {
                     DepartureAndArrivalDetails()
                     BookingAndSeatDetails()
                     GoodToKnowSection()
+                    ArrivalForecast()
                 } header: {
                     flightDetailsHeader
                 }
